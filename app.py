d04e4e9b4d3938709bd759fb350cc0e792cc6106
Alexander Schuppe <alexanderschuppe@AirvonAlexander.fritz.box>
2023-08-02 08:25:17 +0200

    Some Styling to the dashboard, Columns and whitespace

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.set_page_config(
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 1.5rem;
                     padding-bottom: 0rem;
                     padding-left: 2.5rem;
                     padding-right: 5rem;
